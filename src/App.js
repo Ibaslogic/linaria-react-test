fbc9e9f3488c0017f4a8f0901c7c5587c4a10dbe
ibaslogic <ibaskunle@gmail.com>
2022-08-03 01:33:02 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { styled } from '@linaria/react';
+import { css } from '@linaria/core';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <HeadingThree className={style}>Heading III</HeadingThree>
+      <Hello primary="blue">Primary color</Hello>
+      <Hello>Danger</Hello>
+      <div className={flower}>Hello world</div>
+    </>
   );
-}
-
+};
 export default App;
+
+const Hello = styled.p`
+  color: ${({ primary }) => (primary ? 'blue' : 'red')};
+`;
+
+const HeadingThree = styled.h3`
+  background-color: blue;
+  color: green;
+`;
+
+const flower = css`
+  background-color: green;
+  color: red;
+`;
+
+const style = css`
+  color: red;
+`;
--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import './App.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+// export it
+export { globals } from './styles/global';
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
